fee061426eca816581472c7b6f7f090e2399072a
maxzintel <maxzintel@gmail.com>
2023-05-22 23:34:29 -0400

    More work in progress.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head"
+// pages/index.js
 import Header from "../components/Header"
 import Footer from "@/components/Footer"
 import Subscribe from "@/components/Subscribe"
@@ -6,13 +6,17 @@ import Subscribe from "@/components/Subscribe"
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen text-center">
-      <Header></Header>
-      {/* <h1 className='font-franklin text-7xl'>The Most <br></br> Interesting <br></br>Cars on the <br></br> Internet, <br></br>In Your Inbox.</h1> */}
-      <h1 className='font-franklin text-7xl'>Small Penis? <br></br> Have I Got <br></br>A Newsletter For You. </h1>
-      <img src="/964turbo.png" alt="Porsche 993 Turbo" className="mt-16 w-full max-w-md mx-auto"/>
-      <Subscribe></Subscribe>
-      <h2 className='font-folio items-center justify-center text-xl mt-8'>Subscribe now to join our growing gaggle of car lovers and get a twice weekly newsletter showing off the most interesting cars for sale online and telling their stories.</h2>
-      <Footer></Footer>
+      <Header />
+      <div className="flex flex-col md:flex-row items-center justify-center md:space-x-8">
+        {/* <h1 className='font-franklin text-7xl mb-8 md:mb-0'>Small Penis? <br></br> Have I Got <br></br>A Newsletter For You. </h1> */}
+        <h1 className='font-franklin text-7xl mb-8 md:mb-0'>The Most <br></br> Interesting <br></br>Cars on the <br></br> Internet, <br></br>In Your Inbox.</h1>
+        <img src="/964turbo.png" alt="Porsche 993 Turbo" className="w-full max-w-md mx-auto"/>
+      </div>
+      <div className="flex flex-col md:flex-row items-center justify-center md:space-x-8 mt-8">
+        <Subscribe />
+        <h2 className='font-folio text-xl'>Subscribe now to join our growing gaggle of car lovers and get a twice weekly newsletter showing off the most interesting cars for sale online and telling their stories.</h2>
+      </div>
+      <Footer />
     </main>
   )
 }
