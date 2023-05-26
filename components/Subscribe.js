55deaa19f8d4a49f9bccbb266cac74091a5f2823
maxzintel <maxzintel@gmail.com>
2023-05-26 17:35:23 -0400

    Work in progress layout.

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,7 +1,7 @@
 // components/Subscribe.js
 export default function Subscribe() {
   return (
-    <form className="w-full max-w-lg mx-auto mt-8">
+    <form className="font-folio w-full max-w-lg mx-auto mt-8 bg-white p-4 rounded-lg">
       <div className="relative">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg
@@ -16,11 +16,14 @@ export default function Subscribe() {
           </svg>
         </div>
         <input
-          type="text"
+          type="email"
           id="email-address-icon"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="name@flowbite.com"
+          className="bg-white border border-gray-300 text-gray-900 text-lg focus:ring-0 focus-visible:outline-none rounded-lg block w-full pl-10 pr-20 py-3"
+          placeholder="Your Email Address..."
         />
+        <button type="submit" className="absolute inset-y-1 right-1 flex items-center px-4 font-bold text-black bg-yellow-300  hover:bg-yellow-400 rounded-lg">
+          SUBSCRIBE
+        </button>
       </div>
     </form>
   );
--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,17 @@ import Subscribe from "@/components/Subscribe"
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen text-center">
+    <main className="flex flex-col items-center justify-center min-h-screen pt-20">
       <Header />
-      <div className="flex flex-col md:flex-row items-center justify-center md:space-x-8">
-        {/* <h1 className='font-franklin text-7xl mb-8 md:mb-0'>Small Penis? <br></br> Have I Got <br></br>A Newsletter For You. </h1> */}
-        <h1 className='font-franklin text-7xl mb-8 md:mb-0'>The Most <br></br> Interesting <br></br>Cars on the <br></br> Internet, <br></br>In Your Inbox.</h1>
-        <img src="/964turbo.png" alt="Porsche 993 Turbo" className="w-full max-w-md mx-auto"/>
-      </div>
-      <div className="flex flex-col md:flex-row items-center justify-center md:space-x-8 mt-8">
-        <Subscribe />
-        <h2 className='font-folio text-xl'>Subscribe now to join our growing gaggle of car lovers and get a twice weekly newsletter showing off the most interesting cars for sale online and telling their stories.</h2>
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row items-start justify-start md:space-x-8">
+          <h1 className='font-franklin text-7xl mb-8 md:mb-0'>The Most <br></br> Interesting <br></br>Cars on the <br></br> Internet, <br></br>In Your Inbox.</h1>
+          {/* <img src="/964turbo.png" alt="Porsche 993 Turbo" className="w-full max-w-md mx-auto"/> */}
+        </div>
+        <div className="flex flex-col md:flex-row items-start justify-start md:space-x-8 mt-8">
+          <Subscribe />
+          {/* <h2 className='font-folio text-xl max-w-md mx-auto'>Subscribe now to join our growing gaggle of car lovers and get a twice weekly newsletter showing off the most interesting cars for sale online and telling their stories.</h2> */}
+        </div>
       </div>
       <Footer />
     </main>
