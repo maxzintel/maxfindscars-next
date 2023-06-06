ba33f5558b55562ddb1be1a9510d8475541ae23e
maxzintel <maxzintel@gmail.com>
2023-06-05 22:11:25 -0400

    Improved copy and positioning.

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Subscribe from "@/components/Subscribe"
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen pt-20">
+    <main className="flex flex-col items-center justify-center min-h-screen pt-10">
       <Header />
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row items-start justify-start md:space-x-8">
@@ -14,7 +14,7 @@ export default function Home() {
         </div>
         <div className="flex flex-col md:flex-row items-start justify-start md:space-x-8 mt-8">
           {/* <h2 className='font-folio text-xl max-w-md'>Subscribe now to join our growing gaggle of car lovers and get a twice weekly newsletter showing off the most interesting cars for sale online and telling their stories.</h2> */}
-          <h2 className='font-folio text-lg max-w-md'>We make cars for sale entertaining and funny. Join to get our 5 min newsletter showcasing the most interesting cars for sale online.</h2>
+          <h2 className='font-folio text-lg max-w-lg'>We make cars for sale entertaining. Join to get our 5 min newsletter showcasing the most interesting cars for sale online.</h2>
         </div>
         <div className="flex flex-col md:flex-row items-start justify-start md:space-x-8 mt-1">
           <Subscribe />
