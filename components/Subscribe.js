a67fb5c061be9db85fabb737908c50a8997c08c7
maxzintel <maxzintel@gmail.com>
2023-06-09 19:42:07 -0400

    lots of progress.

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,7 +1,7 @@
 // components/Subscribe.js
 export default function Subscribe() {
   return (
-    <form className="font-folio w-full max-w-lg mt-2 bg-white p-4 rounded-lg">
+    <form className="font-folio w-full max-w-lg mt-0 mb-10 bg-white p-3 rounded-lg">
       <div className="relative">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg
@@ -18,7 +18,7 @@ export default function Subscribe() {
         <input
           type="email"
           id="email-address-icon"
-          className="bg-white border border-gray-300 text-gray-900 text-lg focus:ring-0 focus-visible:outline-none rounded-lg block w-full pl-10 pr-20 py-3"
+          className="bg-white border border-gray-300 text-gray-900 text-lg focus:ring-0 focus-visible:outline-none rounded-lg block w-full pl-10 pr-20 py-2"
           placeholder="Your Email Address..."
         />
         <button type="submit" className="absolute inset-y-1 right-1 flex items-center px-4 font-bold text-black bg-yellow-300  hover:bg-yellow-400 rounded-lg">
--- pages/index.js
+++ pages/index.js
@@ -5,38 +5,58 @@ import Subscribe from "@/components/Subscribe";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen pt-10 bg-off-black text-off-white">
+    <main className="flex flex-col items-center justify-center min-h-screen pt-1 bg-off-black text-off-white">
       <Header />
       <div className="container mx-auto">
-        <div className="flex flex-col md:flex-row items-start md:justify-between md:space-x-8">
+        <div className="flex flex-col items-start justify-between space-x-8">
           <div className='w-full md:max-w-4/5'>
-            <h1 className='font-franklin text-8xl mb-2 md:mb-0'>The Most Interesting Cars <br></br>on the Internet,</h1>
-            <div className='mt-1' style={{ 
+            <h1 className='font-franklin md:text-8xl sm:text-6xl text-6xl mb-2 '>The Most Interesting Cars <br></br>on the Internet,</h1>
+            <div className='mt-1 gradient-hover' style={{ 
               background: 'radial-gradient(circle, rgba(251,243,4,1) 0%, rgba(249,180,65,1) 50%, rgba(183,17,22,1) 100%)', 
               borderRadius: '20px',
-              height: '50%',
+              height: '25vh',
               width: '100%',
               display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              backgroundSize: '200% 200%',
+              // padding: '1rem 0', // add padding to ensure content isn't too close to the edges
             }}>
-              <img src="/vector-911.png" alt="Porsche 993 Turbo" className="w-1/2 h-full object-cover"/>
+              <img src="/vector-911.png" alt="Porsche 993 Turbo" className="sm:w-2/3 md:w-2/3 lg:w-2/3 xl:w-2/3 2xl:w-2/3 object-cover mt-6"/>
+
+              <Subscribe/>
             </div>
-            <h1 className='font-franklin text-8xl mb-1 md:mb-0'>In Your Inbox.</h1>
+            <h1 className='font-franklin sm:text-6xl md:text-8xl mb-1 md:mb-0'>In Your Inbox.</h1>
           </div>
         </div>
         <div className="flex flex-col md:flex-row items-start justify-start md:space-x-8 mt-8">
-          <h2 className='font-folio text-lg max-w-lg'>We make cars for sale entertaining. Join to get our 5 min newsletter showcasing the most interesting cars for sale online.</h2>
-        </div>
-        <div className="flex flex-col md:flex-row items-start justify-start md:space-x-8 mt-1">
-          <Subscribe />
+          <div>
+            <h2 className='font-folio text-lg max-w-lg'>We make cars for sale entertaining. Join to get our 5 min newsletter showcasing the most interesting cars for sale online.</h2>
+          </div>
+          <div className='mt-1 gradient-hover' style={{ 
+              background: 'radial-gradient(circle, rgba(251,243,4,1) 0%, rgba(249,180,65,1) 50%, rgba(183,17,22,1) 100%)', 
+              borderRadius: '20px',
+              height: '15vh',
+              width: '50%',
+              // display: 'grid',
+              // gridTemplateColumns: 'repeat(2, 1fr)',
+              // gridTemplateRows: 'repeat(2, 1fr)',
+              // gap: '5px',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundSize: '200% 200%',
+            }}>
+            <div className="grid grid-cols-2 ml-5 mr-5 p-4">
+              <img src="/bmw.png" alt="BMW Logo" className="object-contain w-2/3"/>
+              <img src="/hagerty.png" alt="Hagerty Logo" className="object-contain w-2/3 h-full"/>
+              <img src="/thehustle.png" alt="Morning Brew Logo" className="object-contain w-2/3 h-full"/>
+              <img src="/morningbrew.png" alt="The Hustle Logo" className="object-contain w-2/3 h-full"/>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
     </main>
   );
 }
-
-
-
-{/* <h1 className='font-franklin text-7xl mb-8 md:mb-0 md:mr-auto'>Here's Our Best Shit</h1> */}
