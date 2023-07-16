bca2eb272ccfbe50d3cdcdf4c9b985a63361481c
maxzintel <maxzintel@gmail.com>
2023-07-16 14:29:42 -0400

    Add analytics component

---
+++ components/Analytics.js
@@ -0,0 +1,42 @@
+// GoogleAnalytics.js
+import React from 'react';
+import Head from 'next/head';
+
+const Analytics = () => {
+  return (
+    <Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-PF82H8HXZT"></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-PF82H8HXZT');
+        `}
+      </script>
+      <script>
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-522R2RD');
+        `}
+      </script>
+      <script>
+        {`
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3534872,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </script>
+    </Head>
+  );
+};
+
+export default Analytics;
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
+import Analytics from '@/components/Analytics'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Analytics />
         <link rel="icon" href="/logo.png" />
       </Head>
       <body>
